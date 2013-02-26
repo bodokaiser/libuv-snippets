1b31157d0e4857a1780ccd56f050301f6b7d6ca1
bodokaiser <bodo.kaiser@enabre.com>
2013-02-26 18:57:02 +0100

    commented helloworld main.c

--- helloworld/main.c
+++ helloworld/main.c
@@ -2,13 +2,18 @@
 #include <stdio.h>
 
 int main() {
+    /* initialize a pointer of struct type uv_loop_t */
     uv_loop_t *loop;
     
+    /* assign pointer to return pointer of uv_loop_new */
     loop = uv_loop_new();
 
+    /* print out hello world */
     printf("Hello World\n");
 
+    /* starts the event loop in UV_RUN_DEFAULT mode */
     uv_run(loop, UV_RUN_DEFAULT);
 
+    /* return zero if everything went well */
     return 0;
 }
