f13f7797fa85e9c63521524f937094411c5db00f
bodokaiser <bodo.kaiser@enabre.com>
2013-05-12 18:08:45 +0200

    worked on uv_getaddrinfo

--- doc-examples/dns/uv_getaddrinfo.c
+++ doc-examples/dns/uv_getaddrinfo.c
@@ -1,2 +1,34 @@
-#include "uv.h"
-#include "stdio.h"
+#include <uv.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+uv_loop_t* loop;
+
+uv_getaddrinfo_t* handle;
+
+void getaddrinfo_cb(uv_getaddrinfo_t* handle, int status, 
+        struct addrinfo* response);
+
+const char* name = "localhost";
+
+int main() {
+    loop = uv_default_loop();
+
+    int r = uv_getaddrinfo(loop, handle, getaddrinfo_cb, name, "80", NULL);
+
+    if (r) {
+        printf("Error at dns request: %s.\n",
+                uv_strerror(uv_last_error(loop)));
+    }
+
+    return 0;
+}
+
+void getaddrinfo_cb(uv_getaddrinfo_t* handle, int status,
+        struct addrinfo* response) {
+
+    printf("%s \n", handle->data);
+
+    free(handle);
+    uv_freeaddrinfo(response);
+}
